6030a8121879c04274155b0bd478d1338b0a14d3
Felipe Diniz dos Santos <felipedinizdossantos0@gmail.com>
2023-05-21 20:04:20 -0300

    Inclusion of the carousel menu and its responsiveness

--- src/script/script.js
+++ src/script/script.js
@@ -19,7 +19,6 @@ function menuShow() {
 //Gerencia a troca de slides na HOME
 function showSlides() {
   let widthWindow = window.innerWidth; //Captura o width da tela a cada chamada do intervalo para troca do background dos slides com base no dispostivo do úsuario
-
   for (let i = 0; i < slides.length; i++) {
     slides[i].classList.remove("active");
     dots[i].classList.remove("active");
@@ -54,19 +53,81 @@ var changeBgInterval = setInterval(showSlides, 4000); //Instanciação do interv
 
 dots.forEach((dot, index)=>{ //Limpa o intervalo e dá aos "dots" um escutador que limpa o intervalo e troca o background para o index do respectivo dot clicado
   dot.addEventListener("click", ()=>{
-  clearInterval(changeBgInterval)
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].classList.remove("active");
-    dots[i].classList.remove("active");
-    bgImg[i].classList.remove("active");
-  }
+    clearInterval(changeBgInterval)
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+      dots[i].classList.remove("active");
+      bgImg[i].classList.remove("active");
+    }
 
-  slideIndex = index; 
+    slideIndex = index; 
 
-  bgImg[index].classList.add("active");
-  slides[index].classList.add("active");
-  dots[index].classList.add("active");
+    bgImg[index].classList.add("active");
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
 
-  changeBgInterval = setInterval(showSlides, 4000); //Nova intanciação, pois o clearInterval limpa também o identificador ligado ao intervalo 
+    changeBgInterval = setInterval(showSlides, 4000); //Nova intanciação, pois o clearInterval limpa também o identificador ligado ao intervalo 
+  })
+})
+
+//Código do menu carrosel
+
+const controls = document.querySelectorAll(".control");
+var currentItem = 0;
+const items = document.querySelectorAll(".card");
+const maxItems = items.length;
+
+controls.forEach(control => {
+  control.addEventListener("click", ()=>{
+    const isLeft = control.classList.contains("left")
+    let widthWindow = window.innerWidth; //Captura o width da tela a cada chamada do intervalo para troca do background dos slides com base no dispostivo do úsuario
+
+    function isDeviceWidthSmaller(width){ //Com base na verificação do WIDTH do dispositvo está função retorna true ou false (responsividade)
+      if(width<=735){
+        return true
+      }else{
+        return false
+      }
+    }
+
+    switch(isDeviceWidthSmaller(widthWindow)){ //Switch para definir o valor da troca dos slides pelo botões
+      case true: //Caso o device seja menor 
+        if(isLeft){
+          currentItem -= 1
+        }else{
+          currentItem += 1
+        }
+        break;
+      case false: //Caso o device seja maior
+        if(isLeft){
+          currentItem -= 3
+        }else{
+          currentItem += 3
+        }
+        break;
+      default:
+        console.error("ERRO: WIDTH NÃO INTERPRETADO!")
+    }
+      if(currentItem >= maxItems){ //Reinicia os slides para o inicial, caso ultrapasse o ultimo
+          currentItem = 0
+      }else if(currentItem < 0){
+          currentItem = maxItems - 1
+      }
+      items.forEach(item => {
+        item.classList.remove("current_card")
+      })
+      if(widthWindow <= 735){ //Centraliza o proximo slide a tela. Como só aparece um por vez este comando é necessario para telas menores
+        items[currentItem].scrollIntoView({
+          inline: "center",
+          behavior: "smooth",
+          block: "nearest"
+      })}else{
+          items[currentItem].scrollIntoView({ 
+            inline: "start",
+            behavior: "smooth",
+            block: "nearest"
+      })}
+
+      items[currentItem].classList.add("current_card")
   })
 })
